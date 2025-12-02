6371a5e4d1ee2fe22d806bc1e562509c9cca723b
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2025-12-02 12:38:06 +0100

    feat: make enum for clicks more specific

--- src/Enums/ClickTypeEnum.php
+++ src/Enums/ClickTypeEnum.php
@@ -4,8 +4,7 @@ namespace Thestoragescanner\Payloads\Enums;
 
 enum ClickTypeEnum: string
 {
-    case INBOUND = 'inbound'; // Redirects within the website
-    case OUTBOUND = 'outbound'; // Redirects to links outside the website
-    case SHOW_CONTACT_INFO = 'show_contact_info'; // Reveal phone number or email
+    case OUTBOUND = 'outbound';
+    case SHOW_PHONE = 'show_phone';
+    case SHOW_EMAIL = 'show_email';
 }
-
