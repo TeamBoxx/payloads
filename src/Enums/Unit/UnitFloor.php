d8b90cf94d5b72a63f69ac4fc020dd077d94edec
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2025-12-23 15:20:05 +0100

    fix: upper_floor to upper_floors

--- src/Enums/Unit/UnitFloor.php
+++ src/Enums/Unit/UnitFloor.php
@@ -5,5 +5,5 @@ namespace Thestoragescanner\Payloads\Enums\Unit;
 enum UnitFloor: string
 {
     case GROUND_FLOOR = 'ground_floor';
-    case UPPER_FLOORS = 'upper_floor';
+    case UPPER_FLOORS = 'upper_floors';
 }
