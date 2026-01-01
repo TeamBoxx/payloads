e73908bb5e7dfbc1a8eb7f4d200a09dbbfa9faea
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2026-01-01 15:32:30 +0100

    feat: add locker type

--- src/Enums/Unit/UnitType.php
+++ src/Enums/Unit/UnitType.php
@@ -4,6 +4,7 @@ namespace Thestoragescanner\Payloads\Enums\Unit;
 
 enum UnitType: string
 {
+    case LOCKER = 'locker';
     case SELF_STORAGE = 'self_storage';
     case CONTAINER_STORAGE = 'container_storage';
     case GARAGEBOX = 'garagebox';
