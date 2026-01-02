7ee31fe85d9340239b50d644a8261742dd24ce20
BrunoFigueiredo2k <51383843+BrunoFigueiredo2k@users.noreply.github.com>
2026-01-02 13:55:59 +0100

    revert

--- src/Enums/Unit/UnitType.php
+++ src/Enums/Unit/UnitType.php
@@ -8,14 +8,4 @@ enum UnitType: string
     case SELF_STORAGE = 'self_storage';
     case CONTAINER_STORAGE = 'container_storage';
     case GARAGEBOX = 'garagebox';
-
-    public function bootstrapIcon(): string
-    {
-        return match ($this) {
-            self::LOCKER => 'bi-lock',
-            self::SELF_STORAGE => 'bi-box-seam',
-            self::CONTAINER_STORAGE => 'bi-truck',
-            self::GARAGEBOX => 'bi-house-door',
-        };
-    }
 }
