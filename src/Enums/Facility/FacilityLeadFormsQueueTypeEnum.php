7da8680652f7159ce98f1f26bd0488f17202d186
BrunoFigueiredo2k <51383843+BrunoFigueiredo2k@users.noreply.github.com>
2026-03-20 16:18:28 +0100

    change order user name prefix consistent with other naming

--- src/Enums/Facility/FacilityLeadFormsQueueTypeEnum.php
+++ src/Enums/Facility/FacilityLeadFormsQueueTypeEnum.php
@@ -4,7 +4,7 @@ namespace Thestoragescanner\Payloads\Enums\Facility;
 
 enum FacilityLeadFormsQueueTypeEnum: string
 {
-    case USER_MAIL = 'user_mail';
+    case USER_MAIL = 'mail_user';
     case REMINDER_MAIL_USER = 'reminder_mail_user';
     case MAIL_PROVIDER = 'mail_provider';
     case REMINDER_MAIL_PROVIDER_1 = 'reminder_mail_provider_1';
