111b5ae7afef588107e542015b85fbed24ee711a
BrunoFigueiredo2k <51383843+BrunoFigueiredo2k@users.noreply.github.com>
2026-03-20 14:56:36 +0100

    Create FacilityLeadFormsQueueTypeEnum.php

---
+++ src/Enums/Facility/FacilityLeadFormsQueueTypeEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Thestoragescanner\Payloads\Enums\Facility;
+
+enum FacilityLeadFormsQueueTypeEnum: string
+{
+    case USER_MAIL = 'user_mail';
+    case REMINDER_MAIL_USER = 'reminder_mail_user';
+    case MAIL_PROVIDER = 'mail_provider';
+    case REMINDER_MAIL_PROVIDER_1 = 'reminder_mail_provider_1';
+    case REMINDER_MAIL_PROVIDER_2 = 'reminder_mail_provider_2';
+}
